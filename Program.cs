103389abe64b6288010af883278c634fec787dab
fanur <makhiyanov.ff@gmail.com>
2022-07-28 15:09:41 +0300

    Внутри цикла for отбираем значения массива соответствущие условию задачи и выводим в консоль

--- Program.cs
+++ Program.cs
@@ -4,3 +4,16 @@ Console.Write($"[{str}] -> ");
 
 for (int i = 0; i < myArray.Length; i++)
 
+{
+    if (myArray[i].Length <= 3)
+    {
+        if (i != myArray.Length - 1)
+        {
+            Console.Write($"{myArray[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{myArray[i]}");
+        }
+    }
+}

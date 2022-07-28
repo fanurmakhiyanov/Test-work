1ba48c227b740645953d474b2a00884399e9311e
fanur <makhiyanov.ff@gmail.com>
2022-07-28 15:06:09 +0300

    Создание цикла for для перебора каждого элемента массива на предмет соответствия заданному условию

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 ﻿string[] myArray = { ";)", "Moscow", "GeekBrains", "GB", "99" };
-string str = string.Join( ", ", myArray );
+string str = string.Join(", ", myArray);
 Console.Write($"[{str}] -> ");
+
+for (int i = 0; i < myArray.Length; i++)
+
